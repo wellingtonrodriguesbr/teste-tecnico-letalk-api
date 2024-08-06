72bd8523f53361e756c9306396ab00dc4015ef56
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-08-06 11:59:55 -0300

    chore: generate new migration

--- prisma/migrations/20240804114138_create_tables/migration.sql
+++ prisma/migrations/20240804114138_create_tables/migration.sql
@@ -4,12 +4,12 @@ CREATE TABLE "loans" (
     "customer_document_number" TEXT NOT NULL,
     "customer_birth_date" DATETIME NOT NULL,
     "customer_state" TEXT NOT NULL,
-    "loan_amount_requested" INTEGER NOT NULL,
+    "loan_amount_requested" TEXT NOT NULL,
     "interest_rate" REAL NOT NULL,
-    "desired_installment_amount" INTEGER NOT NULL,
+    "desired_installment_amount" TEXT NOT NULL,
     "installments" INTEGER NOT NULL,
-    "total_interest_rate_amount" INTEGER NOT NULL,
-    "total_amount" INTEGER NOT NULL,
+    "total_interest_rate_amount" TEXT NOT NULL,
+    "total_amount" TEXT NOT NULL,
     "loan_made_effective" BOOLEAN NOT NULL DEFAULT false,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -17,10 +17,10 @@ CREATE TABLE "loans" (
 -- CreateTable
 CREATE TABLE "loan_installments" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "outstanding_balance" INTEGER NOT NULL,
-    "interest" INTEGER NOT NULL,
-    "adjusted_balance" INTEGER NOT NULL,
-    "installment_amount" INTEGER NOT NULL,
+    "outstanding_balance" TEXT NOT NULL,
+    "interest" TEXT NOT NULL,
+    "adjusted_balance" TEXT NOT NULL,
+    "installment_amount" TEXT NOT NULL,
     "due_date" DATETIME NOT NULL,
     "loanId" TEXT NOT NULL,
     CONSTRAINT "loan_installments_loanId_fkey" FOREIGN KEY ("loanId") REFERENCES "loans" ("id") ON DELETE CASCADE ON UPDATE CASCADE
